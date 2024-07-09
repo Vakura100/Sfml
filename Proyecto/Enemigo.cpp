9195d51c64a6b206a302bf569c5588e54a17a354
Vakura100 <150973571+Vakura100@users.noreply.github.com>
2024-07-09 12:42:54 -0600

    Delete Proyecto directory

--- Proyecto/Enemigo.cpp
+++
@@ -1,9 +0,0 @@
-#include "Enemigo.h"
-
-Enemigo::Enemigo(std::string nombre, int ataque, int defensa, int salud)
-        : Entidad(nombre, ataque, defensa, salud) {}
-
-void Enemigo::mostrar() const {
-    std::cout << "Enemigo:" << std::endl;
-    Entidad::mostrar();
-}
